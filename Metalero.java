76444831a0095b40d851c45438978298cd92e623
Toño Tejera <tonodevep@gmail.com>
2025-05-01 17:04:06 +0100

    Añadiendo la clase "Metalero"

---
+++ Metalero.java
@@ -0,0 +1,28 @@
+public class Metalero extends PersonajeFisico implements Defendible {
+    private String guitarra;
+    private boolean escudo;
+
+    public Metalero(String nombre, int nivel, double salud, int fuerza, String guitarra) {
+        super(nombre, nivel, salud, fuerza);
+        this.guitarra = guitarra;
+        this.escudo = true;
+    }
+
+    @Override
+    public void atacar() {
+        System.out.println(nombre + " ataca con su guitarra " + guitarra + " y le quita un " + fuerza + " de daño ");
+    }
+
+    @Override
+    public void defender() {
+        if (escudo) {
+            System.out.println(nombre + " se defiende tocando Slayer");
+        } else {
+            System.out.println(nombre + " no tiene la guitarra afinada para defenderse");
+        }
+    }
+
+    public void tocarGuitarra() {
+        System.out.println(nombre + " está tocando un solo con su " + guitarra);
+    }
+}
