20f2a0e05e3632e15ed9bc2705c8ff2aae89a83a
Toño Tejera <tonodevep@gmail.com>
2025-05-01 19:09:04 +0100

    Update Magico.java

--- Interfaces/Magico.java
+++ Interfaces/Magico.java
@@ -1,5 +1,8 @@
-package Interfaces;
+package Interfaces; // Define el paquete donde se encuentra esta interfaz
 
+// Interfaz que define un comportamiento mágico para los personajes
 public interface Magico {
+
+    // Método que debe ser implementado por cualquier clase que use esta interfaz
     void lanzarHechizo();
 }
