4e1b0f31556c42472ea53ed316116de362d745cc
Toño Tejera <tonodevep@gmail.com>
2025-05-01 19:05:38 +0100

    Update Policia.java

--- Personajes/Policia.java
+++ Personajes/Policia.java
@@ -1,29 +1,35 @@
-package Personajes;
+package Personajes; // Define el paquete donde se encuentra esta clase
+
+// Importa las interfaces necesarias y la clase base PersonajeFisico
 import Interfaces.Pistolero;
 import Personajes.base.PersonajeFisico;
 
+// Define la clase Policia, que extiende PersonajeFisico e implementa la interfaz Pistolero
 public class Policia extends PersonajeFisico implements Pistolero {
     
-    private int balas = 2;
-    
-    
+    private int balas = 2; // Atributo que guarda el número de balas que tiene el policía
+
+    // Constructor de la clase Policia, inicializa los atributos
     public Policia(String nombre, int nivel, double salud, int fuerza) {
-        super(nombre, nivel, salud, fuerza);
+        super(nombre, nivel, salud, fuerza); // Llama al constructor de la clase base (PersonajeFisico)
     }
 
+    // Implementación del método atacar (del interface Pistolero)
     @Override
     public void atacar() {
         System.out.println("El policia " + nombre + " ataca con su cuchillo quitando " + fuerza + " de daño ");
     }
 
+    // Implementación del método disparar (del interface Pistolero)
     @Override
-    public void disparar(){
-        if (balas > 0){
+    public void disparar() {
+        if (balas > 0) {  // Si el policía tiene balas
             System.out.println("El policia " + nombre + " dispara con su pistola al enemigo y le quita " + fuerza + " de daño");
-            balas--;
-        }else{
-            System.out.println("A el policia " + nombre + " no le quedan balas");
-            System.out.println(nombre + " pierde su turno");
+            balas--;  // Resta una bala después de disparar
+        } else {  // Si no tiene balas
+            System.out.println("Al policia " + nombre + " no le quedan balas");
+            System.out.println(nombre + " pierde su turno");  // El policía pierde su turno por falta de balas
         }
     }
 }
+
