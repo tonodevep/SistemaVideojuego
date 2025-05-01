4afa508290ce4353cb94a2a350d1832d2d6410f0
Toño Tejera <tonodevep@gmail.com>
2025-05-01 19:08:37 +0100

    Update Defendible.java

--- Interfaces/Defendible.java
+++ Interfaces/Defendible.java
@@ -1,5 +1,9 @@
-package Interfaces;
+package Interfaces; // Define el paquete donde se encuentra esta interfaz
 
+// Interfaz que define un comportamiento defensivo para los personajes
 public interface Defendible {
+
+    // Método que debe ser implementado por cualquier clase que use esta interfaz
     void defender();
 }
+
