0e9e16008e1790f81c507c18b51e93171b7dfdc9
Juniuun <sarafox_2006@hotmail.com>
2025-05-01 18:04:48 +0100

    Creación de la clase "Policia"
    Agregación del rol "policía" para el roaster de personajes del juego.

---
+++ Policia.java
@@ -0,0 +1,27 @@
+import Interfaces.Pistolero;
+
+public class Policia extends PersonajeFisico implements Pistolero {
+    
+    private int balas = 2;
+    
+    
+    public Policia(String nombre, int nivel, double salud, int fuerza) {
+        super(nombre, nivel, salud, fuerza);
+    }
+
+    @Override
+    public void atacar() {
+        System.out.println("El policia " + nombre + " ataca con su cuchillo quitando " + fuerza + " de daño ");
+    }
+
+    @Override
+    public void disparar(){
+        if (balas > 0){
+            System.out.println("El policia " + nombre + " dispara con su pistola al enemigo y le quita " + fuerza + " de daño");
+            balas--;
+        }else{
+            System.out.println("A el policia " + nombre + " no le quedan balas");
+            System.out.println(nombre + " pierde su turno");
+        }
+    }
+}
