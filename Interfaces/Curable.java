4725852bb054a5b2af3f09be7867ba623453d8c5
Toño Tejera <tonodevep@gmail.com>
2025-05-01 19:08:12 +0100

    Update Curable.java

--- Interfaces/Curable.java
+++ Interfaces/Curable.java
@@ -1,6 +1,8 @@
-package Interfaces;
+package Interfaces; // Define el paquete donde se encuentra esta interfaz
 
+// Interfaz que define un comportamiento curativo para los personajes
 public interface Curable {
+
+    // Método que debe ser implementado por cualquier clase que use esta interfaz
     void curar();
 }
-
