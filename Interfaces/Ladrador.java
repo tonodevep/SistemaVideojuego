aeb24b1ae7cbff2efed75bf912d6199ae05d58c0
Juniuun <sarafox_2006@hotmail.com>
2025-05-02 12:15:35 +0100

    Update Ladrador.java

--- Interfaces/Ladrador.java
+++ Interfaces/Ladrador.java
@@ -1,5 +1,5 @@
-package Interfaces;
+package Interfaces; // Define el paquete donde se encuentra esta interfaz
 
-public interface Ladrador {
-    void alarido();
+public interface Ladrador { // Interfaz que define un comportamiento para los personajes
+    void alarido(); // Método que debe ser implementado por cualquier clase que use esta interfaz
 }
