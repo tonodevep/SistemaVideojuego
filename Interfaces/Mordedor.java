3ba4fb75243dd922fbb8200588292a32d1a832ff
Juniuun <sarafox_2006@hotmail.com>
2025-05-02 12:14:43 +0100

    Update Mordedor.java

--- Interfaces/Mordedor.java
+++ Interfaces/Mordedor.java
@@ -1,5 +1,5 @@
-package Interfaces;
+package Interfaces; //define el paquete en donde se encuentra la interfaz
 
-public interface Mordedor {
-    void morder();
+public interface Mordedor { // Interfaz que define un comportamiento para los personajes
+    void morder(); // Método que debe ser implementado por cualquier clase que use esta interfaz
 }
